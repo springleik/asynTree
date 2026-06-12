f4d809bc9ef586083d162ed84d75d673b463bb26
Mark Williamsen <mark@williamsonic.com>
2026-06-12 20:51:03 +0200

    Added Java folder.

---
+++ Java/toyMotor.java
@@ -0,0 +1,8 @@
+// Toy Motor Demo Program
+// Demonstrate abstract syntax tree technology
+
+public class toyMotor {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
